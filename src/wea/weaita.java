216b1ff3f15fa6b8010f6c091a5a2df1ea267c1f
Sojiroh <jorge.kakashi@gmail.com>
2015-01-04 23:13:02 -0300

    inicial

---
+++ src/wea/weaita.java
@@ -0,0 +1,36 @@
+package wea;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+
+
+
+public class weaita {
+	
+	public static void main(String[] args) throws Exception {
+		String pathDir = "/Users/Sojiroh/Downloads/xmlcesion";
+		Path pathDirectorio = Paths.get(pathDir);
+		try (DirectoryStream<Path> pathFiles = Files.newDirectoryStream(pathDirectorio)) {
+			
+			for (Path p: pathFiles) {
+				
+				System.out.println(p.getFileName());
+				
+	
+				
+			}
+		} catch (IOException e) {
+		
+			throw new Exception("Error al procesar directorio: " + e.getMessage());
+		}
+		
+
+	}
+	
+
+}
